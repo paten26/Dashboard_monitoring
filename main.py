f94615c02c8ad46eb711c4665c76472213668679
LUKAS YULIANTO <paten26@gmail.com>
2024-07-29 13:09:34 +0700

    aplikasi gempa

---
+++ main.py
@@ -0,0 +1,27 @@
+"""
+APLIKASI UPDATE GEMPA BMKG
+"""
+
+
+def ekstraksi_data(): #mendefinisikan fungsi ekstraksi data
+"""
+Gempabumi Terkini
+Tanggal 28 Juli 2024,
+Waktu 18:20:31 WIB
+Magnitudo 5.6
+Kedalaman 144 km
+Lokasi 6.31 LS - 130.20 BT
+Pusat gempa : 221 km BaratLaut TANIMBAR
+Keterangan : tidak berpotensi TSUNAMI
+"""
+    pass
+
+
+def tampilkan_data(result): #mendefinisikan fungsi tampilkan data
+    pass
+
+
+if __name__ == "__main__" :
+    print('Aplikasi Utama')
+    result = ekstraksi_data() #fungsi untuk mengekstraksi data
+    tampilkan_data(result) #fungsi untuk menampilkan data
